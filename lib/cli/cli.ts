0798acb73d55f51ce2812a76e7ca41401e6a9373
Lukas Reichmann <lukas@reichmann-info.de>
2020-03-08 20:48:32 +0100

    Added CLI.

---
+++ lib/cli/cli.ts
@@ -0,0 +1,16 @@
+import yargs from 'yargs';
+import Generate from './generate';
+
+yargs
+    .command(
+        '$0',
+        'Shows help, when no sub-command is specified.',
+        () => {},
+        () => {
+            yargs.showHelp();
+        }
+    )
+    .command('generate', 'Generate a client from a DDF.', Generate.build, Generate.execute)
+    .help()
+    .alias('help', 'h')
+    .alias('version', 'v').argv;
---
+++ lib/cli/generate.ts
@@ -0,0 +1,76 @@
+import packageJSON from '../../package.json';
+import PreProcessor from '../core/pre-processing/pre-processor';
+import logger from '../core/logger.js';
+import yargs from 'yargs';
+import path from 'path';
+
+namespace Generate {
+    export function build(command: yargs.Argv) {
+        command
+            .version(packageJSON.version)
+            .option('phase', {
+                alias: 'p',
+                choices: PreProcessor.ALL_PHASES,
+                default: PreProcessor.ALL_PHASES[PreProcessor.ALL_PHASES.length - 1],
+                description: 'Pre-Process until a certain phase, then exit. Used for debugging.'
+            })
+            .option('verbosity', {
+                choices: ['silent', 'minimal', 'debug'],
+                default: 'silent',
+                description: 'Set logging level.'
+            })
+            .option('log-ddf', {
+                alias: 'l',
+                boolean: true,
+                default: false,
+                description: 'Log pre-processed DDF. Used for debugging.'
+            })
+            .help()
+            .alias('help', 'h')
+            .alias('version', 'v');
+    }
+
+    export async function execute(argv: yargs.Arguments) {
+        let filePath = processPath(argv._[1] ? argv._[1] : 'database-definition.json');
+
+        setLoggingLevel(argv.verbosity as string);
+
+        try {
+            const preProcessor = new PreProcessor(filePath);
+            const ddf = await preProcessor.run(argv.phase as PreProcessor.Phase);
+
+            if (argv.logDdf) {
+                logger.info(JSON.stringify(ddf, null, 4));
+            }
+        } catch (error) {
+            logger.error(error);
+        }
+    }
+
+    // turn 'dir/file.json' relatives paths into './dir/file.json'
+    // required, b/c otherwise, the Loader will load from the wrong directory
+    function processPath(p: string): string {
+        if (!path.isAbsolute(p) && !p.startsWith('.')) {
+            return `.${path.sep}${p}`;
+        }
+
+        return p;
+    }
+
+    function setLoggingLevel(level: string) {
+        switch (level) {
+            case 'silent':
+            default:
+                logger.silent = true;
+                break;
+            case 'minimal':
+                logger.level = 'info';
+                break;
+            case 'debug':
+                logger.level = 'debug';
+                break;
+        }
+    }
+}
+
+export = Generate;
